acc3dda08ed0ae4adf18c76384ab4e34c0433396
jgkim <jgkim999@gmail.com>
2023-12-10 21:40:23 +0900

    16장

--- person.py
+++ person.py
@@ -6,4 +6,7 @@ class Person:
 
     def greeting(self):
         print('안녕하세요. 저는 {0}입니다. 나이는 {1}살이고 사는 곳은 {2}입니다.'.format(self.name, self.age, self.address))
-    
+
+
+a = Person("Monster", 11, "USA")
+a.greeting()
